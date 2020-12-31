10acb1e5f49ab7713fe60827a29759e901b20d3f
Vrund Patel <vrundpat@buffalo.edu>
2020-12-30 20:06:09 -0500

    Fixed Camera movement to support generic WASD

--- src/RenderEngine/Renderer.java
+++ src/RenderEngine/Renderer.java
@@ -1,20 +1,37 @@
 package RenderEngine;
 
+import Entities.Entity;
 import Models.RawModel;
 import Models.TexturedModel;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL13;
-import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GL30;
+import Shaders.StaticShader;
+import Tools.Maths;
+import org.lwjgl.opengl.*;
+import org.lwjgl.util.vector.Matrix4f;
 
 public class Renderer {
 
+    private static final float FOV = 70;
+    private static final float NEAR_PLANE = 0.1f;
+    private static final float FAR_PLANE = 1000;
+
+    private Matrix4f projectionMatrix;
+
+    // Constructor will now create a projectionMatrix upon initialization
+    public Renderer(StaticShader shader) {
+        createProjectionMatrix();
+        shader.start();
+        shader.loadProjectionMatrix(projectionMatrix);
+        shader.stop();
+    }
+
     public void prepare() {
-        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT); // Clear the screen from the previous frame
+        GL11.glEnable(GL11.GL_DEPTH_TEST); // Allow OpenGL to monitor overlapping vertices and render accordingly
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT); // Clear the screen from the previous frame & allow depth buffer testing
         GL11.glClearColor(1, 0, 0, 1); // Fill the window with a simple red color
     }
 
-    public void render(TexturedModel texturedModel) {
+    public void render(Entity entity, StaticShader shader) {
+        TexturedModel texturedModel = entity.getModel();
         RawModel model = texturedModel.getRawModel();
         // Bind the VBO
         GL30.glBindVertexArray(model.getVaoId());
@@ -22,6 +39,11 @@ public class Renderer {
 
         // Enable the texture VBO & activate and bind the texture
         GL20.glEnableVertexAttribArray(1);
+
+        // Create a transformation matrix for this entity
+        Matrix4f transformationMatrix = Maths.createTransformationMatrix(entity.getPosition(), entity.getRotX(), entity.getRotY(), entity.getRotZ(), entity.getScale());
+        shader.loadTransformationMatrix(transformationMatrix); // Load the matrix into the shader
+
         GL13.glActiveTexture(GL13.GL_TEXTURE0);
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, texturedModel.getTexture().getTextureID());
 
@@ -34,4 +56,19 @@ public class Renderer {
         GL30.glBindVertexArray(0); // Unbind the vertices VBO
     }
 
+    /* Project math converted to code using an online reference, forgot to copy link so now I don't wanna go looking for it...*/
+    private void createProjectionMatrix() {
+        float aspectRatio = (float) Display.getWidth() / (float) Display.getHeight();
+        float y_scale = (float) ((1f / Math.tan(Math.toRadians(FOV / 2f))) * aspectRatio);
+        float x_scale = y_scale / aspectRatio;
+        float frustum_length = FAR_PLANE - NEAR_PLANE;
+
+        projectionMatrix = new Matrix4f();
+        projectionMatrix.m00 = x_scale;
+        projectionMatrix.m11 = y_scale;
+        projectionMatrix.m22 = -((FAR_PLANE + NEAR_PLANE) / frustum_length);
+        projectionMatrix.m23 = -1;
+        projectionMatrix.m32 = -((2 * NEAR_PLANE * FAR_PLANE) / frustum_length);
+        projectionMatrix.m33 = 0;
+    }
 }
